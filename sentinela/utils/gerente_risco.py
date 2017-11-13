14a51497a19a1c18d0d20576776f55b52e49ec1d
IvanBrasilico <brasilico.ivan@gmail.com>
2017-11-13 19:56:27 -0200

    testes funcionais

--- sentinela/utils/gerente_risco.py
+++ sentinela/utils/gerente_risco.py
@@ -2,6 +2,7 @@
 de risco cadastrados nos dados. Utiliza pandas para realizar filtragem
 """
 import csv
+import os
 from collections import defaultdict
 
 import pandas as pd
@@ -56,14 +57,29 @@ class GerenteRisco():
         """Configura os parametros de risco ativos"""
         self.riscosativos.pop(parametrorisco.nome_campo, None)
 
-    def aplica_risco(self, lista):
+    def clear_risco(self):
+        """Zera os parametros de risco ativos"""
+        self.riscosativos = {}
+
+    def aplica_risco(self, lista=None, arquivo=None):
         """Compara a linha de título da lista recebida com a lista de nomes
         de campo que possuem parâmetros de risco ativos. Após, chama para cada
          campo encontrado a função de filtragem
         lista: Lista a ser filtrada, primeira linha deve conter os nomes dos
             campos idênticos aos definidos no nome_campo do parâmetro de risco
             cadastrado.
+        arquivo: Arquivo csv contendo a lista a ser filtrada
         """
+        mensagem = "Arquivo não fornecido!"
+        if arquivo:
+            mensagem = "Lista não fornecida!"
+            with open(arquivo, 'r', encoding='iso-8859-1') as arq:
+                reader = csv.reader(arq)
+                lista = [linha for linha in reader]
+
+        if not lista:
+            raise AttributeError("Erro! " + mensagem)
+
         for r in range(len(lista)):
             lista[r] = list(map(str.strip, lista[r]))
         headers = set(lista[0])
@@ -85,23 +101,26 @@ class GerenteRisco():
         print(result)
         return result
 
-    def parametros_tocsv(self):
+    def parametros_tocsv(self, dest_path='/tmp'):
         """Salva os parâmetros adicionados a um gerente em um arquivo csv
         Ver também: parametros_fromcsv"""
-        for campo, valores in self.riscosativos:
+        for campo, dict_filtros in self.riscosativos.items():
             lista = []
-            lista.append('valor,tipo_filtro')
-            for tipo_filtro, valor in valores:
-                lista.append(valor + ',' + tipo_filtro.name)
-            with open(campo + '.csv', 'w') as f:
+            lista.append(('valor', 'tipo_filtro'))
+            for tipo_filtro, lista_filtros in dict_filtros.items():
+                for valor in lista_filtros:
+                    lista.append((valor, tipo_filtro.name))
+            with open(os.path.join(dest_path, campo + '.csv'), 'w') as f:
                 writer = csv.writer(f)
                 writer.writerows(lista)
 
-    def parametros_fromcsv(self, campo, session=None, lista=None):
+    def parametros_fromcsv(self, campo, session=None, lista=None, dest_path='/tmp'):
         """Abre um arquivo csv, recupera parâmetros configurados nele,
         adiciona à configuração do gerente e **também adiciona ao Banco de
         Dados ativo** caso não existam nele ainda. Para isso é preciso
         passar a session como parâmetro, senão cria apenas na memória
+        Pode receber uma lista no lugar de um arquivo csv (como implementado
+        em import_named_csv)
         Ver também: parametros_tocsv, import_named_csv
         campo: nome do campo a ser filtrado e deve ser também
             o nome do arquivo .csv
@@ -113,7 +132,7 @@ class GerenteRisco():
         O arquivo .csv ou a lista DEVEM estar no formato valor, tipo_filtro
         """
         if not lista:
-            with open(campo + '.csv', 'r') as f:
+            with open(os.path.join(dest_path, campo + '.csv'), 'r') as f:
                 reader = csv.reader(f)
                 lista = [linha for linha in reader]
                 lista = lista[1:]
@@ -128,7 +147,7 @@ class GerenteRisco():
                     valor = session.query(ValorParametro).filter(
                         ValorParametro.valor == linha[0],
                         ValorParametro.risco_id == parametro.id).first()
-                    valor.tipo_filtro = Filtro(linha[1])
+                    valor.tipo_filtro = Filtro[linha[1]]
                     session.merge(valor)
                     if not valor:
                         valor = ValorParametro(linha[0].strip(),
@@ -141,7 +160,7 @@ class GerenteRisco():
         else:
             dict_filtros = defaultdict(list)
             for linha in lista:
-                dict_filtros[Filtro(linha[1])].append(linha[0])
+                dict_filtros[Filtro[linha[1]]].append(linha[0])
             self.riscosativos[campo] = dict_filtros
 
     def import_named_csv(self, arquivo, session=None, filtro=Filtro.igual):
@@ -154,12 +173,13 @@ class GerenteRisco():
         filtro: Tipo de filtro a ser assumido como padrão"""
         with open(arquivo, 'r') as f:
             reader = csv.reader(f)
-            cabecalho = reader.readline()
+            cabecalho = next(reader)
             listas = defaultdict(list)
             for linha in reader:
                 ind = 0
                 for coluna in linha:
-                    listas[cabecalho[ind]].append([coluna.strip(), filtro])
+                    listas[cabecalho[ind].strip()].append(
+                        [coluna.strip(), filtro.name])
                     ind += 1
-        for key, value in listas:
+        for key, value in listas.items():
             self.parametros_fromcsv(key, session, value)
