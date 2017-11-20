8061e0a53793bcab72bb8a9c8a28826f9f3be9e2
IvanBrasilico <brasilico.ivan@gmail.com>
2017-11-20 14:00:23 -0200

    Interface  aplica_risco prototipo - final

--- sentinela/tests/exemplos/test_carga.py
+++ sentinela/tests/exemplos/test_carga.py
@@ -6,12 +6,43 @@ import os
 import tempfile
 import unittest
 
-from sentinela.models.models import (Base, Filtro, MySession, ParametroRisco,
-                                     ValorParametro)
+from sentinela.models.models import (Base, BaseOriginal, Filtro, MySession,
+                                     ParametroRisco, ValorParametro)
 from sentinela.utils.csv_handlers import sch_processing
 from sentinela.utils.gerente_risco import GerenteRisco
 
+# Configurar nesta linha uma base do Sistema Carga E
 CARGA_ZIP_TEST = '/home/ivan/Downloads/P.zip'
+# Comentar a linha abaixo para testar com uma base real
+CARGA_ZIP_TEST = 'sentinela/tests/tests.zip'
+
+
+def carrega_carga(session):
+    base = BaseOriginal('CARGA')
+    session.add(base)
+    risco = ParametroRisco('CPFCNPJConsignatario', 'CNPJ do Consignatario')
+    session.add(risco)
+    # Adicionar um CNPJ que exista na extração, para testar...
+    valor = ValorParametro('42581413000157', Filtro.igual)
+    session.add(valor)
+    risco.valores.append(valor)
+    session.merge(risco)
+    session.commit()
+
+    risco2 = ParametroRisco('DescricaoMercadoria', 'Descrição')
+    session.add(risco2)
+    # Adicionar uma mercadoria que exista na extração, para testar...
+    valor2 = ValorParametro('PILLOW', Filtro.contem)
+    session.add(valor2)
+    session.commit()
+    risco2.valores.append(valor2)
+    session.merge(risco2)
+    session.commit()
+    base.parametros.append(risco)
+    base.parametros.append(risco2)
+    session.merge(base)
+    session.commit()
+    return base
 
 
 class TestModel(unittest.TestCase):
@@ -39,37 +70,33 @@ class TestModel(unittest.TestCase):
         # O pytest só chama os métodos começados com test
         #
         ###################
-
-        risco = ParametroRisco('CPFCNPJConsignatario', 'CNPJ do Consignatario')
-        self.session.add(risco)
-        # Adicionar um CNPJ que exista na extração, para testar...
-        valor = ValorParametro('42581413000157', Filtro.igual)
-        self.session.add(valor)
-        risco.valores.append(valor)
-        self.session.merge(risco)
-
-        risco2 = ParametroRisco('DescricaoMercadoria', 'Descrição')
-        self.session.add(risco2)
-        # Adicionar uma mercadoria que exista na extração, para testar...
-        valor2 = ValorParametro('PILLOW', Filtro.contem)
-        self.session.add(valor2)
-        self.session.commit()
-        risco2.valores.append(valor2)
-        self.session.merge(risco2)
-        self.session.commit()
-        # Comentado para não rodar no Servidor
-        """filenames = sch_processing(CARGA_ZIP_TEST)
+        filenames = sch_processing(CARGA_ZIP_TEST)
         print(filenames)
         assert not filenames is None
         # assert(len(filenames) == 14)
+        base = carrega_carga(self.session)
         gerente = GerenteRisco()
-        gerente.add_risco(risco)
-        print(risco.valores)
-        with open(filenames[4][0]) as show_the_head:
+        gerente.set_base(base)
+        print(base.parametros[0].valores)
+        # Preferencialmente vai tentar processar o arquivo de conhecimentos
+        # Se não houver, pega o primeiro da lista mesmo
+        ind = 0
+        for index, filename in enumerate(filenames):
+            if filename[0].find("Conhecimento"):
+                ind = index
+                break
+        with open(filenames[ind][0]) as show_the_head:
             head = [next(show_the_head) for x in range(5)]
         print(head)
-        result = gerente.aplica_risco(arquivo=filenames[4][0])
+        result = gerente.aplica_risco(arquivo=filenames[ind][0])
         print(result)
         # assert False  # To view output, uncomment this
-        """
+
         # TODO:  Testar gerente.aplica_juncao() em uma base do Carga
+
+
+if __name__ == '__main__':
+    # Cria no banco atualmente configurado os objetos da função carrega_carga
+    # Apenas para praticidade durante o período inicial de testes
+    mysession = MySession(Base, test=False)
+    carrega_carga(mysession.session)
