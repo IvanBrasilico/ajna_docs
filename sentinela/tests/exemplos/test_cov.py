56f14b2cb6e5630abe181a384fb952525052e75e
Adauto Nóbrega <adauto204@gmail.com>
2017-12-14 18:17:22 -0200

    Tela de risco, DePara, estudos da tela login

--- sentinela/tests/exemplos/test_cov.py
+++ sentinela/tests/exemplos/test_cov.py
@@ -5,7 +5,7 @@ import os
 import tempfile
 import unittest
 
-from sentinela.models.models import Base, MySession
+from sentinela.models.models import Base, BaseOrigem, DePara, MySession
 from sentinela.utils.gerente_risco import GerenteRisco
 
 PLANILHA_TEST = '/home/ivan/Downloads/planilhaBTP.csv'
@@ -42,11 +42,34 @@ class TestModel(unittest.TestCase):
         gerente.parametros_fromcsv('alimento', path=self.tmpdir)
         # , path='.')
 
+    def test_planilhaCOV(self):
+        base = BaseOrigem('Planilha_BTP')
+        self.session.add(base)
+        self.session.commit()
+        depara1 = DePara('antigo1', 'novo1', base)
+        self.session.add(depara1)
+        depara2 = DePara('antigo2', 'novo2', base)
+        self.session.add(depara2)
+        self.session.commit()
+        de_para_dict = {depara.titulo_ant:depara.titulo_novo for depara in base.deparas}
+        print(de_para_dict)
+        # lista_old = 
+        lista = muda_titulos_lista(lista_old,
+                                        de_para_dict)
+        for old, new in de_para_dict:
+            assert old in ''.join(lista_old[0])
+            assert new not in ''.join(lista_old[0])
+        for old, new in de_para_dict:
+            assert new in ''.join(lista[0])
+
+        assert False
+
 
 if __name__ == '__main__':
     # Cria no banco atualmente configurado os objetos de teste
     # Apenas para praticidade durante o período inicial de testes
     mysession = MySession(Base, test=False)
-    gerente = GerenteRisco()
-    gerente.import_named_csv(CSV_NAMEDRISCO_TEST, session=mysession.session)
-    gerente.cria_base('PLANILHA_COV', session=mysession.session)
+    print("Atenção, entrando em Base REAL!!!!")
+    # gerente = GerenteRisco()
+    # gerente.import_named_csv(CSV_NAMEDRISCO_TEST, session=mysession.session)
+    # gerente.cria_base('PLANILHA_COV', session=mysession.session)
