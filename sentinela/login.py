b125b942dfe8510788b20c3daf3fd5f238806f9c
IvanBrasilico <brasilico.ivan@gmail.com>
2017-12-15 14:57:11 -0200

    login

--- sentinela/login.py
+++ sentinela/login.py
@@ -1,33 +1,10 @@
 from flask_login import LoginManager, login_user
-
-from Flask import Response, abort, redirect, render_template, request, url_for
-from sentinela.app import app
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-users_repository = {'ajna': 'ajna'}
+from flask import Response, abort, redirect, render_template, request, url_for
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        registeredUser = users_repository.get_user(username)
-        print('Users ' + str(users_repository.users))
-        print('Register user %s , password %s' %
-              (registeredUser.username, registeredUser.password))
-        if registeredUser is not None and registeredUser.password == password:
-            print('Logged in..')
-            login_user(registeredUser)
-            return redirect(url_for('home'))
-        else:
-            return abort(401)
-    else:
-        return render_template('index.html')
 
 
+"""
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -38,6 +15,7 @@ def register():
         return Response('Registered Successfully')
     else:
         return render_template('index.html')
+"""
 
 
 class User():
@@ -63,4 +41,4 @@ def page_not_found(e):
 
 @login_manager.user_loader
 def load_user(userid):
-    return users_repository.get_user_by_id(userid)
+    return users_repository.get(userid)
