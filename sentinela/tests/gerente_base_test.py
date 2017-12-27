fd97fbed1420da0ccc4740e54009ee1a0d66dd2e
IvanBrasilico <brasilico.ivan@gmail.com>
2017-12-27 21:59:03 -0200

    treeview

--- sentinela/tests/gerente_base_test.py
+++ sentinela/tests/gerente_base_test.py
@@ -1,13 +1,21 @@
+"""Testa gerente_base, um navegador simples de bases de dados
+Caso chamado diretamente, cria uma base carga de testes
+Testes estão sendo feitos na base padrão carga, mas gerente_base
+deve funcionar com qualquer base que implemente o mesmo padrão que
+a base carga.
+"""
 import unittest
 
-from sentinela.models.carga import Base, Escala, MySession
+from sentinela.models.carga import Base, MySession
+from sentinela.models.carga import (AtracDesatracEscala, ContainerVazio,
+                                    Escala, EscalaManifesto, Manifesto)
 from sentinela.utils.gerente_base import Filtro, GerenteBase
 
 
 class TestModel(unittest.TestCase):
     def setUp(self):
         self.gerente = GerenteBase()
-        mysession = MySession(Base, test=False)
+        mysession = MySession(Base, test=False, nomebase='cargatest.db')
         self.dbsession = mysession.session
         self.engine = mysession.engine
 
@@ -42,8 +50,50 @@ class TestModel(unittest.TestCase):
         assert dados is not None"""
 
     def test_tree(self):
+        # TODO: criar base carga de testes
+        # pass
         escala = self.dbsession.query(Escala).filter(
-            Escala.Escala == '15000365317').first()
+            Escala.Escala == 'E-1').first()
         if escala:
-            print(self.gerente.recursive_tree(escala))
-        # assert False
+            lista = self.gerente.recursive_tree(escala)
+            print(lista)
+            with open('tree.html', 'w') as html:
+                for linha in lista:
+                    html.write('{}\n'.format(linha))
+        assert False
+
+
+if __name__ == '__main__':
+    mysession = MySession(Base, test=False, nomebase='cargatest.db')
+    dbsession = mysession.session
+    engine = mysession.engine
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    escala = Escala()
+    escala.Escala = 'E-1'
+    escala.CNPJAgenciaNavegacao = 'E-C01'
+    escala.CodigoIMO = 'E-IMO01'
+    dbsession.add(escala)
+    atracacao = AtracDesatracEscala()
+    atracacao.Escala = 'E-1'
+    atracacao.CodigoTerminal = 'A-T01'
+    dbsession.add(atracacao)
+    manifesto = Manifesto()
+    manifesto.Manifesto = 'M-2'
+    dbsession.add(manifesto)
+    escalamanifesto = EscalaManifesto()
+    escalamanifesto.Escala = 'E-1'
+    escalamanifesto.Manifesto = 'M-2'
+    dbsession.add(escalamanifesto)
+    vazio = ContainerVazio()
+    vazio.Manifesto = 'M-2'
+    vazio.Container = 'C-C01'
+    vazio.Capacidade = 'C-40'
+    dbsession.add(vazio)
+    vazio2 = ContainerVazio()
+    vazio2.Manifesto = 'M-2'
+    vazio2.Container = 'C-C02'
+    vazio2.Capacidade = 'C-40'
+    dbsession.add(vazio2)
+    dbsession.commit()
+    unittest.main()
