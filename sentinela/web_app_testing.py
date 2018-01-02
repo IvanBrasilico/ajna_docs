44a6a3804fdc9b69bb86a24d7a624d78054c4103
Adauto Nóbrega <adauto204@gmail.com>
2018-01-02 17:05:05 -0200

    flake8 + isort

--- sentinela/web_app_testing.py
+++ sentinela/web_app_testing.py
@@ -3,12 +3,12 @@ https://docs.pylonsproject.org/projects/webtest/en/latest/testapp.html
 Usage: python sentinela/web_app_testing.py
 Needs a running server to test
 """
-import unittest
 import os
-
-os.environ['HTTP_SERVER'] = 'http://localhost:5000'
+import unittest
 
 from sentinela.tests.app_test import FlaskTestCase
+
+os.environ['HTTP_SERVER'] = 'http://localhost:5000'
 
 if __name__ == '__main__':
     test = FlaskTestCase
