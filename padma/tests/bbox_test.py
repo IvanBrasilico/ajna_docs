f4ab2ab9ee3e64afb03e02c8569f9c4faf8d97ea
Brasilico <brasilico.ivan@gmail.com>
2018-02-05 11:47:20 -0200

    bbox

---
+++ padma/tests/bbox_test.py
@@ -0,0 +1,34 @@
+# Tescases for models/vazios/vazios.py
+import os
+import unittest
+
+from PIL import Image
+
+from models.bbox.bbox import NaiveModel, RetinaModel
+from keras.preprocessing.image import img_to_array
+
+IMAGE = os.path.join(os.path.dirname(__file__), 'stamp1.jpg')
+
+
+class TestModel(unittest.TestCase):
+    def setUp(self):
+        self.image = img_to_array(Image.open(IMAGE))
+        self.naive = NaiveModel()
+        self.retina = RetinaModel()
+
+    def tearDown(self):
+        pass
+
+    def test_naive(self):
+        preds = self.naive.predict(self.image)
+        print(preds)
+        assert False
+
+    def test_resnet(self):
+        preds = self.retina.predict(self.image)
+        print(preds)
+        assert False
+
+
+if __name__ == '__main__':
+    unittest.main()
