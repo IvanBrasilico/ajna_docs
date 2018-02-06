b6fa97601cb3f531943d638cb5bcb398ec39fce2
Brasilico <brasilico.ivan@gmail.com>
2018-02-06 10:05:00 -0200

    --

--- image_aq/tests/bsonimage_test.py
+++ image_aq/tests/bsonimage_test.py
@@ -1,5 +1,28 @@
+import datetime
+import unittest
+import os
+
 from image_aq.models.bsonimage import BsonImage
 
+IMG_FOLDER = 'padma/tests/'
 
-bson = BsonImage('test', conteiner='MSKU')
 
+class TestModel(unittest.TestCase):
+    def setUp(self):
+        self.mongosession = None
+
+    def tearDown(self):
+        pass
+
+    def test1_bson(self):
+        self._bson = BsonImage(
+            filename = os.path.join(IMG_FOLDER, 'stamp1.jpg'),
+            chave = 'MSKU123',
+            origem = 0,
+            data = datetime.datetime.now()
+
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
