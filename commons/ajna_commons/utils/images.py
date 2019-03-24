8a240810d27936e052bfe530d1d86edf05f8d312
brasilico.ivan@gmail.com <Iv@n1234>
2019-03-24 08:27:46 -0300

    generate_batch

--- commons/ajna_commons/utils/images.py
+++ commons/ajna_commons/utils/images.py
@@ -71,3 +71,32 @@ def get_imagens_recortadas(db, _id):
                                     'Erro: %s\n bbox:%s\n imagem:%s' %
                                     (str(err), bbox, _id), exc_info=True)
     return images
+
+
+def get_cursor(db, filtro, projection=None, limit=None):
+    if projection:
+        cursor = db['fs.files'].find(filtro, projection)
+    else:
+        cursor = db['fs.files'].find(filtro)
+    if limit:
+        cursor = cursor[:limit]
+    return cursor
+
+
+def generate_batch(db, filtro, projection=None, batch_size=32, limit=None):
+    """a generator for batches, so model.fit_generator can be used. """
+    cursor = get_cursor(db, filtro, projection, limit)
+    while True:
+        images = []
+        rows = []
+        i = 0
+        while i < batch_size:
+            try:
+                row = next(cursor)
+            except StopIteration:
+                break
+            imgs = get_imagens_recortadas(db, row['_id'])
+            images.append(imgs)
+            rows.append(row)
+            i += 1
+        yield images, rows
